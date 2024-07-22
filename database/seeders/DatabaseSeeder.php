08d0a0d1bb22ad4ff8286ae60ed1e3b02607c16b
Neddy AP <neddy1298@gmail.com>
2024-07-22 21:19:34 +0700

    Update 'merk_kendaraan' column length in 'create_kendaraan' migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,26 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\Kendaraan;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // PermohonanKtp::factory(10)->create();
-
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'role' => 'admin',
         ]);
 
-        // Kendaraan::factory(50)->create();
+        $path = storage_path('app/public/tbl_kendaraan.sql');
+        DB::unprepared(file_get_contents($path));
     }
 }
