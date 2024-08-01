573217ca5a6f55c64d28c1a8cd8e1a77a6fa99b3
Neddy AP <neddy1298@gmail.com>
2024-08-01 12:59:00 +0700

    refactor: Update BelanjaController to include 'maintenance' relationship in index and create methods

--- resources/views/belanja/index.blade.php
+++ resources/views/belanja/index.blade.php
@@ -57,7 +57,7 @@
                                 @foreach ($belanjas as $index => $belanja)
                                     <tr>
                                         <td>{{ $index + 1 }}</td>
-                                        <td>{{ $belanja->nomor_registrasi }}</td>
+                                        <td>{{ $belanja->maintenance->kendaraan->nomor_registrasi }}</td>
                                         <td>Rp. {{ number_format($belanja->belanja_bahan_bakar_minyak + $belanja->belanja_pelumas_mesin + $belanja->belanja_suku_cadang, 0, ',', '.') }}</td>
                                         <td>
                                             {{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}
@@ -66,11 +66,11 @@
                                         <td>
                                             <button type="button" class="btn btn-primary btn-icon show-details"
                                                 data-bs-toggle="modal" data-bs-target="#detailModal"
-                                                data-nomor-registrasi="{{ $belanja->nomor_registrasi }}"
+                                                data-nomor-registrasi="{{ $belanja->maintenance->kendaraan->nomor_registrasi }}"
                                                 data-bahan-bakar-minyak="{{ $belanja->belanja_bahan_bakar_minyak }}"
                                                 data-pelumas-mesin="{{ $belanja->belanja_pelumas_mesin }}"
                                                 data-suku-cadang="{{ $belanja->belanja_suku_cadang }}"
-                                                data-total-belanja="{{ $belanja->belanja_bahan_bakar_minyak + $belanja->belanja_pelumas_mesin + $belanja->belanja_suku_cadang }}"
+                                                data-total-belanja="{{ $belanja->total_belanja }}"
                                                 data-tanggal-belanja="{{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}"
                                                 data-keterangan="{{ $belanja->keterangan }}"
                                                 data-id="{{ $belanja->id }}">
