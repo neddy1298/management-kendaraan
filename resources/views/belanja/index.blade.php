6fd5cdaa4949f886c3bc59b2203ca71fa63ce063
Neddy AP <neddy1298@gmail.com>
2024-08-02 18:07:30 +0700

    minor

--- resources/views/belanja/index.blade.php
+++ resources/views/belanja/index.blade.php
@@ -11,172 +11,166 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="row">
-            <div class="col-xxl-4 col-sm-6 col-12">
-                <div class="stats-tile">
-                    <div class="sale-icon shade-green">
-                        <h4 class="text-white">Rp</h4>
-                    </div>
-                    <div class="sale-details">
-                        <h3 class="text-green">
-                            {{ number_format($belanja_periode, 0, ',', '.') }}.00
-                        </h3>
-                        <p>Total Belanja 
-                            @if(isset($dateRange))
-                                {{ \Carbon\Carbon::createFromFormat('d/m/Y', trim(explode(' - ', $dateRange)[0]))->format('d M Y') }} 
-                                - 
-                                {{ \Carbon\Carbon::createFromFormat('d/m/Y', trim(explode(' - ', $dateRange)[1]))->format('d M Y') }}
-                            @else
-                                Semua Periode
-                            @endif
-                        </p>
-                    </div>
+    <div class="col-12">
+        <div class="stats-tile">
+            <div class="sale-icon shade-green">
+                <h4 class="text-white">Rp</h4>
+            </div>
+            <div class="sale-details">
+                <h3 class="text-green">
+                    {{ number_format($belanja_periode, 0, ',', '.') }}.00
+                </h3>
+                <p>Total Belanja
+                    @if (isset($dateRange))
+                        {{ \Carbon\Carbon::createFromFormat('d/m/Y', trim(explode(' - ', $dateRange)[0]))->format('d M Y') }}
+                        -
+                        {{ \Carbon\Carbon::createFromFormat('d/m/Y', trim(explode(' - ', $dateRange)[1]))->format('d M Y') }}
+                    @else
+                        Semua Periode
+                    @endif
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Card start -->
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="custom-btn-group col-6">
+                    <a href="{{ route('belanja.create') }}" class="btn btn-warning">
+                        <i class="bi bi-pencil-square"></i> Tambah Baru
+                    </a>
+                    <a href="{{ route('belanja.printAll') }}" class="btn btn-primary" target="_blank">
+                        <i class="bi bi-printer"></i> Cetak
+                    </a>
+                    @php
+                        $message =
+                            "Contoh message yang akan dikirim ke WA\n-1. lorem ipsum dolor sit amet consectetur adipiscing elit\n2. lorem ipsum dolor sit amet consectetur adipiscing elit\n3. lorem ipsum dolor sit amet consectetur adipiscing elit\n4. lorem ipsum dolor sit amet consectetur adipiscing elit\n5. lorem ipsum dolor sit amet consectetur adipiscing elit";
+                    @endphp
+                    <a href="{{ route('send-wa', ['message' => $message]) }}" class="btn btn-success" target="_blank">
+                        <i class="bi bi-whatsapp"></i> Kirim WA
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- Card start -->
-
-
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="custom-btn-group col-6">
-                        <a href="{{ route('belanja.create') }}" class="btn btn-warning">
-                            <i class="bi bi-pencil-square"></i> Tambah Baru
-                        </a>
-                        <a href="{{ route('belanja.printAll') }}" class="btn btn-primary" target="_blank">
-                            <i class="bi bi-printer"></i> Cetak
-                        </a>
-                        @php
-                            $message =
-                                "Contoh message yang akan dikirim ke WA\n-1. lorem ipsum dolor sit amet consectetur adipiscing elit\n2. lorem ipsum dolor sit amet consectetur adipiscing elit\n3. lorem ipsum dolor sit amet consectetur adipiscing elit\n4. lorem ipsum dolor sit amet consectetur adipiscing elit\n5. lorem ipsum dolor sit amet consectetur adipiscing elit";
-                        @endphp
-                        <a href="{{ route('send-wa', ['message' => $message]) }}" class="btn btn-success" target="_blank">
-                            <i class="bi bi-whatsapp"></i> Kirim WA
-                        </a>
-                    </div>
+    </div>
+    <div class="col-sm-12 col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="m-0">
                 </div>
-            </div>
-        </div>
-        <div class="col-sm-12 col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="m-0">
-                    </div>
-                    <form action="{{ route('belanja.index') }}" method="GET"
-                        class="row row-cols-lg-auto g-3 align-items-center">
-                        <div class="col-12">
-                            <div class="input-group">
-                                <span class="input-group-text">
-                                    <i class="bi bi-calendar2"></i>
-                                </span>
-                                <input type="text" class="form-control datepicker-range" name="date-range">
-                            </div>
+                <form action="{{ route('belanja.index') }}" method="GET"
+                    class="row row-cols-lg-auto g-3 align-items-center">
+                    <div class="col-12">
+                        <div class="input-group">
+                            <span class="input-group-text">
+                                <i class="bi bi-calendar2"></i>
+                            </span>
+                            <input type="text" class="form-control datepicker-range" name="date-range">
                         </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-primary">Filter</button>
-                            <a class="btn btn-dark btn-icon btn-sm" href="{{ route('belanja.index') }}"
-                                data-bs-toggle="tooltip" data-bs-placement="top" title="Reset">
-                                <i class="bi bi-arrow-counterclockwise"></i>
-                            </a>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Filter</button>
+                        <a class="btn btn-dark btn-icon btn-sm" href="{{ route('belanja.index') }}"
+                            data-bs-toggle="tooltip" data-bs-placement="top" title="Reset">
+                            <i class="bi bi-arrow-counterclockwise"></i>
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
 
-        <div class="col-sm-12 col-12">
-            @if (session('success'))
-                @include('partials.alert', ['type' => 'success', 'message' => session('success')])
-            @elseif (session('error'))
-                @include('partials.alert', ['type' => 'danger', 'message' => session('error')])
-            @endif
+    <div class="col-sm-12 col-12">
+        @if (session('success'))
+            @include('partials.alert', ['type' => 'success', 'message' => session('success')])
+        @elseif (session('error'))
+            @include('partials.alert', ['type' => 'danger', 'message' => session('error')])
+        @endif
 
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-title">Data Kendaraan</div>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table id="highlightRowColumn" class="table custom-table text-center v-middle">
-                            <thead>
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title">Data Kendaraan</div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="highlightRowColumn" class="table custom-table text-center v-middle">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nomor Registrasi</th>
+                                <th>Total Belanja</th>
+                                <th>Tanggal Belanja</th>
+                                <th>Keterangan</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($belanjas as $index => $belanja)
                                 <tr>
-                                    <th>No</th>
-                                    <th>Nomor Registrasi</th>
-                                    <th>Total Belanja</th>
-                                    <th>Tanggal Belanja</th>
-                                    <th>Keterangan</th>
-                                    <th>Action</th>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $belanja->maintenance->kendaraan->nomor_registrasi }}</td>
+                                    <td>Rp.
+                                        {{ number_format($belanja->belanja_bahan_bakar_minyak + $belanja->belanja_pelumas_mesin + $belanja->belanja_suku_cadang, 0, ',', '.') }}
+                                    </td>
+                                    <td>
+                                        {{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}
+                                    </td>
+                                    <td>{{ $belanja->keterangan }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary btn-icon show-details"
+                                            data-bs-toggle="modal" data-bs-target="#detailModal"
+                                            data-nomor-registrasi="{{ $belanja->maintenance->kendaraan->nomor_registrasi }}"
+                                            data-bahan-bakar-minyak="{{ $belanja->belanja_bahan_bakar_minyak }}"
+                                            data-pelumas-mesin="{{ $belanja->belanja_pelumas_mesin }}"
+                                            data-suku-cadang="{{ $belanja->belanja_suku_cadang }}"
+                                            data-total-belanja="{{ $belanja->total_belanja }}"
+                                            data-tanggal-belanja="{{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}"
+                                            data-keterangan="{{ $belanja->keterangan }}" data-id="{{ $belanja->id }}">
+                                            <i class="bi bi-search"></i>
+                                        </button>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($belanjas as $index => $belanja)
-                                    <tr>
-                                        <td>{{ $index + 1 }}</td>
-                                        <td>{{ $belanja->maintenance->kendaraan->nomor_registrasi }}</td>
-                                        <td>Rp.
-                                            {{ number_format($belanja->belanja_bahan_bakar_minyak + $belanja->belanja_pelumas_mesin + $belanja->belanja_suku_cadang, 0, ',', '.') }}
-                                        </td>
-                                        <td>
-                                            {{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}
-                                        </td>
-                                        <td>{{ $belanja->keterangan }}</td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary btn-icon show-details"
-                                                data-bs-toggle="modal" data-bs-target="#detailModal"
-                                                data-nomor-registrasi="{{ $belanja->maintenance->kendaraan->nomor_registrasi }}"
-                                                data-bahan-bakar-minyak="{{ $belanja->belanja_bahan_bakar_minyak }}"
-                                                data-pelumas-mesin="{{ $belanja->belanja_pelumas_mesin }}"
-                                                data-suku-cadang="{{ $belanja->belanja_suku_cadang }}"
-                                                data-total-belanja="{{ $belanja->total_belanja }}"
-                                                data-tanggal-belanja="{{ \Carbon\Carbon::parse($belanja->tanggal_belanja)->translatedFormat('d F Y') }}"
-                                                data-keterangan="{{ $belanja->keterangan }}"
-                                                data-id="{{ $belanja->id }}">
-                                                <i class="bi bi-search"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- ModalBelanja  -->
-        <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="detailModalLabel">Detail Belanja</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p><strong>Nomor Registrasi:</strong> <span id="modalNomorRegistrasi"></span></p>
-                        <p><strong>Tanggal Belanja:</strong> <span id="modalTanggalBelanja"></span></p>
-                        <p><strong>Belanja BBM:</strong> Rp. <span id="modalBahanBakarMinyak"></span>.00</p>
-                        <p><strong>Belanja Pelumas:</strong> Rp. <span id="modalPelumasMesin"></span>.00</p>
-                        <p><strong>Belanja Suku Cadang:</strong> Rp. <span id="modalSukuCadang"></span>.00</p>
-                        <p><strong>Total Belanja:</strong> Rp. <span id="modalTotalBelanja"></span>.00</p>
-                        <p><strong>Keterangan:</strong> <span id="modalKeterangan"></span></p>
-                    </div>
-                    <div class="modal-footer">
-                        <form action="" method="POST" style="display: inline-block" id="modalDeleteBelanja">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger btn-icon btn-sm"
-                                onclick="return confirm('Kamu yakin ingin menghapus data belanja?')"
-                                data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus">
-                                <i class="bi bi-trash"></i>
-                            </button>
-                        </form>
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+    <!-- ModalBelanja  -->
+    <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="detailModalLabel">Detail Belanja</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p><strong>Nomor Registrasi:</strong> <span id="modalNomorRegistrasi"></span></p>
+                    <p><strong>Tanggal Belanja:</strong> <span id="modalTanggalBelanja"></span></p>
+                    <p><strong>Belanja BBM:</strong> Rp. <span id="modalBahanBakarMinyak"></span>.00</p>
+                    <p><strong>Belanja Pelumas:</strong> Rp. <span id="modalPelumasMesin"></span>.00</p>
+                    <p><strong>Belanja Suku Cadang:</strong> Rp. <span id="modalSukuCadang"></span>.00</p>
+                    <p><strong>Total Belanja:</strong> Rp. <span id="modalTotalBelanja"></span>.00</p>
+                    <p><strong>Keterangan:</strong> <span id="modalKeterangan"></span></p>
+                </div>
+                <div class="modal-footer">
+                    <form action="" method="POST" style="display: inline-block" id="modalDeleteBelanja">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-icon btn-sm"
+                            onclick="return confirm('Kamu yakin ingin menghapus data belanja?')" data-bs-toggle="tooltip"
+                            data-bs-placement="top" title="Hapus">
+                            <i class="bi bi-trash"></i>
+                        </button>
+                    </form>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
 
