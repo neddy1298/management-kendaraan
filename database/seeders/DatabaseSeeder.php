6dbce9ae525434791a53bf1c6d30acd3ca8d4b65
SkyRaazz <115127423+SkyRaazz@users.noreply.github.com>
2024-08-07 10:33:52 +0700

    minor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,18 +28,18 @@ class DatabaseSeeder extends Seeder
 
         // $path = storage_path('app/public/kendaraans.sql');
         // DB::unprepared(file_get_contents($path));
-        PaguAnggaran::factory()->count(1)->create();
-        LaporanTahunan::factory()->count(1)->create();
-        LaporanBulanan::factory()->count(12)->create();
-        MasterAnggaran::factory()->count(1)->create();
-        GroupAnggaran::factory()->count(10)->create();
-        UnitKerja::factory()->count(10)->create();
-        Kendaraan::factory()
-            ->count(100)
-            ->has(Maintenance::factory())
-            ->create();
-        Belanja::factory()
-            ->count(100)
-            ->create();
+        // PaguAnggaran::factory()->count(1)->create();
+        // LaporanTahunan::factory()->count(1)->create();
+        // LaporanBulanan::factory()->count(12)->create();
+        // MasterAnggaran::factory()->count(1)->create();
+        // GroupAnggaran::factory()->count(10)->create();
+        // UnitKerja::factory()->count(10)->create();
+        // Kendaraan::factory()
+        //     ->count(100)
+        //     ->has(Maintenance::factory())
+        //     ->create();
+        // Belanja::factory()
+        //     ->count(100)
+        //     ->create();
     }
 }
