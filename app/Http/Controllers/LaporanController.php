2e502658389fad48914265902a4b006f52ac3c90
Isafird <muhamadisafirdaus123@gmail.com>
2024-08-07 10:20:07 +0700

    p

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,12 +2,210 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\GroupAnggaran;
+use App\Models\MasterAnggaran;
+use App\Models\PaguAnggaran;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 
 class LaporanController extends Controller
 {
     public function index()
     {
         return view('laporan.index');
+    }
+
+    public function exportToExcel()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $masteAnggarans = MasterAnggaran::all();
+        $paguAnggarans = PaguAnggaran::all();
+        $groupAnggarans = GroupAnggaran::all();
+
+        // Judul
+        $titles = [
+            'A1' => 'PEMERINTAH KOTA BOGOR',
+            'A2' => 'KARTU KENDALI KEGIATAN',
+            'A3' => 'TAHUN ANGGARAN ' . date('Y'),
+            'A5' => 'Sub Unit                : DINAS PERHUBUNGAN KOTA BOGOR',
+            'A6' => 'Nama Program    : PROGRAM PENUNJANG URUSAN PEMERINTAHAN DAERAH KABUPATEN/KOTA',
+            'A7' => 'Nama Kegiatan    : Pemeliharaan Barang Milik Daerah Penunjang Urusan Pemerintahan Daerah',
+            'A8' => 'Sub Kegiatan        : Penyediaan Jasa Pemeliharaan, Biaya Pemeliharaan, Pajak, dan Perizinan Kendaraan Dinas Operasional atau Lapangan',
+            'A9' => 'Nama PPTK           : Firza Firani Rizal, S.Kom, M.Ak.',
+            'A10' => 'Bulan                     : ' . date('F Y')
+        ];
+        foreach ($titles as $cell => $value) {
+            $sheet->setCellValue($cell, $value);
+        }
+
+        // Merge Cell Judul
+        for ($row = 1; $row <= 10; $row++) {
+            $sheet->mergeCells("A$row:N$row");
+        }
+
+        // Style JUdul
+        $sheet->getStyle('A1:A3')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => 12,
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+        ]);
+
+        // Tabel Header
+        $headers = [
+            'A12' => 'No',
+            'B12' => 'Kode Rekening',
+            'C12' => 'Nama Rekening',
+            'D12' => 'Pagu Anggaran Kegiatan (Rp)',
+            'D14' => '    UP/GU/TU    ',
+            'E14' => '    LS    ',
+            'F12' => 'Realisasi Kegiatan (SP2D) (Rp)',
+            'F13' => 'S/D Bulan Lalu',
+            'F14' => '    UP/GU/TU    ',
+            'G14' => '    LS    ',
+            'H13' => 'Bulan Ini',
+            'H14' => '    UP    ',
+            'I14' => '    GU    ',
+            'J14' => '    TU    ',
+            'K14' => '    LS    ',
+            'L13' => 'S/D Bulan Ini',
+            'L14' => '    UP/GU/TU    ',
+            'M14' => '    LS    ',
+            'N12' => 'Sisa Pagu Anggaran (Rp)'
+        ];
+        foreach ($headers as $cell => $value) {
+            $sheet->setCellValue($cell, $value);
+        }
+
+        // Style Tabel
+        $styleHeading1 = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FF808080',
+                ],
+            ],
+        ];
+        $styleHeading2 = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FFFFA500',
+                ],
+            ],
+        ];
+
+        // Rupiah format
+        $rupiahFormat = [
+            'numberFormat' => [
+                'formatCode' => '[$Rp-421] #,##0',
+            ],
+        ];
+        $sheet->getStyle('D17:N17' . ($row - 1))->applyFromArray($rupiahFormat);
+
+        // Looping isi tabel
+        $row = 17;
+        $index = 1;
+
+        // Heading 1
+        foreach ($paguAnggarans as $paguAnggaran) {
+            $sheet->setCellValue("A$row", $index);
+            $sheet->setCellValue("B$row", $paguAnggaran->kode_rekening);
+            $sheet->getStyle("A$row:N$row")->applyFromArray($styleHeading1);
+            $row++;
+            $index++;
+
+            // Heading 2
+            foreach ($masteAnggarans as $masteAnggaran) {
+                $row++;
+                $sheet->setCellValue("B$row", $masteAnggaran->kode_rekening);
+                $sheet->getStyle("B$row:N$row")->applyFromArray($styleHeading2);
+                $row++;
+
+                // Heading 3
+                foreach ($groupAnggarans as $groupAnggaran) {
+                    $sheet->setCellValue("C$row", $groupAnggaran->nama_group);
+                    $sheet->setCellValue("D$row", $groupAnggaran->total_anggaran);
+                    $row++;
+                }
+            }
+        }
+
+        // Merge Cell Tabel Header
+        $mergeCells = [
+            'A12:A14',
+            'B12:B14',
+            'C12:C14',
+            'D12:E13',
+            'F12:M12',
+            'F13:G13',
+            'H13:K13',
+            'L13:M13',
+            'N12:N14'
+        ];
+        foreach ($mergeCells as $mergeCell) {
+            $sheet->mergeCells($mergeCell);
+        }
+
+        // Autosize
+        foreach (range('A', 'N') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+
+        // Style Header Tabel
+        $headerStyleArray = [
+            'font' => [
+                'bold' => true,
+                'color' => ['argb' => '000000'],
+            ],
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => ['argb' => '4863A0'],
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_MEDIUM,
+                    'color' => ['argb' => 'FF000000'],
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+        ];
+
+        // Style kode rekening
+        $sheet->getStyle('A17:B' . $sheet->getHighestRow())->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => 12,
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+        ]);
+
+        $sheet->getStyle('A12:N14')->applyFromArray($headerStyleArray);
+
+        // Nama file
+        $writer = new Xlsx($spreadsheet);
+        $fileName = 'Data Maintenance Kendaraan Dinas Perhubungan Kota Bogor ' . date('d-F-Y') . '.xlsx';
+
+        // Headers untuk unduhan file
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $fileName . '"');
+        header('Cache-Control: max-age=0');
+
+        $writer->save('php://output');
+        exit();
     }
 }
--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Belanja;
+use App\Models\GroupAnggaran;
 use App\Models\Maintenance;
+use App\Models\MasterAnggaran;
+use App\Models\PaguAnggaran;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -111,21 +114,33 @@ class MaintenanceController extends Controller
     {
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
+        $masteAnggarans = MasterAnggaran::all();
+        $paguAnggarans = PaguAnggaran::all();
+        $groupAnggarans = GroupAnggaran::all();
 
-        $maintenances = Maintenance::all();
+        // Judul
+        $titles = [
+            'A1' => 'PEMERINTAH KOTA BOGOR',
+            'A2' => 'KARTU KENDALI KEGIATAN',
+            'A3' => 'TAHUN ANGGARAN ' . date('Y'),
+            'A5' => 'Sub Unit                : DINAS PERHUBUNGAN KOTA BOGOR',
+            'A6' => 'Nama Program    : PROGRAM PENUNJANG URUSAN PEMERINTAHAN DAERAH KABUPATEN/KOTA',
+            'A7' => 'Nama Kegiatan    : Pemeliharaan Barang Milik Daerah Penunjang Urusan Pemerintahan Daerah',
+            'A8' => 'Sub Kegiatan        : Penyediaan Jasa Pemeliharaan, Biaya Pemeliharaan, Pajak, dan Perizinan Kendaraan Dinas Operasional atau Lapangan',
+            'A9' => 'Nama PPTK           : Firza Firani Rizal, S.Kom, M.Ak.',
+            'A10' => 'Bulan                     : ' . date('F Y')
+        ];
+        foreach ($titles as $cell => $value) {
+            $sheet->setCellValue($cell, $value);
+        }
 
+        // Merge Cell Judul
+        for ($row = 1; $row <= 10; $row++) {
+            $sheet->mergeCells("A$row:N$row");
+        }
 
-        // judul
-        $sheet->setCellValue('A1', 'RKA BBM DAN PEMELIHARAAN 2024');
-        $sheet->setCellValue('A2', 'Penyediaan Jasa Pemeliharaan, Biaya Pemeliharaan, Pajak dan Perizinan Kendaraan Dinas Operasional atau Lapangan');
-
-        $highestColumn = 'J';
-        $sheet->mergeCells('A1:' . $highestColumn . '1');
-        $sheet->mergeCells('A2:' . $highestColumn . '2');
-
-
-        // style
-        $sheet->getStyle('A1:A2')->applyFromArray([
+        // Style JUdul
+        $sheet->getStyle('A1:A3')->applyFromArray([
             'font' => [
                 'bold' => true,
                 'size' => 12,
@@ -135,210 +150,154 @@ class MaintenanceController extends Controller
                 'vertical' => Alignment::VERTICAL_CENTER,
             ],
         ]);
-        foreach (range('A2', 'J2') as $columnID) {
-            $sheet->getColumnDimension($columnID)->setAutoSize(true);
-        }
 
-
-        // Bagian pertama dari tabel
-        $header1 = [
-            'A4' => 'No',
-            'B4' => 'Kode Rekening',
-            'C4' => 'Uraian',
-            'D4' => 'Volume',
-            'E4' => 'Satuan',
-            'F4' => 'Volume',
-            'G4' => 'Satuan',
-            'H4' => 'Harga',
-            'I4' => 'Jumlah',
-            'J4' => 'Vol'
+        // Tabel Header
+        $headers = [
+            'A12' => 'No',
+            'B12' => 'Kode Rekening',
+            'C12' => 'Nama Rekening',
+            'D12' => 'Pagu Anggaran Kegiatan (Rp)',
+            'D14' => '    UP/GU/TU    ',
+            'E14' => '    LS    ',
+            'F12' => 'Realisasi Kegiatan (SP2D) (Rp)',
+            'F13' => 'S/D Bulan Lalu',
+            'F14' => '    UP/GU/TU    ',
+            'G14' => '    LS    ',
+            'H13' => 'Bulan Ini',
+            'H14' => '    UP    ',
+            'I14' => '    GU    ',
+            'J14' => '    TU    ',
+            'K14' => '    LS    ',
+            'L13' => 'S/D Bulan Ini',
+            'L14' => '    UP/GU/TU    ',
+            'M14' => '    LS    ',
+            'N12' => 'Sisa Pagu Anggaran (Rp)'
         ];
-
-        // Bagian bulanan dari tabel
-        $months = [
-            'L4' => 'Januari', 'M4' => 'SPJ', 'N4' => 'Silpa',
-            'P4' => 'Februari', 'Q4' => 'SPJ', 'R4' => 'Silpa',
-            'T4' => 'Maret', 'U4' => 'SPJ', 'V4' => 'Silpa',
-            'X4' => 'April', 'Y4' => 'SPJ', 'Z4' => 'Silpa',
-            'AB4' => 'Mei', 'AC4' => 'SPJ', 'AD4' => 'Silpa',
-            'AF4' => 'Juni', 'AG4' => 'SPJ', 'AH4' => 'Silpa',
-            'AJ4' => 'Juli', 'AK4' => 'SPJ', 'AL4' => 'Silpa',
-            'AN4' => 'Agustus', 'AO4' => 'SPJ', 'AP4' => 'Silpa',
-            'AR4' => 'September', 'AS4' => 'SPJ', 'AT4' => 'Silpa',
-            'AV4' => 'Oktober', 'AW4' => 'SPJ', 'AX4' => 'Silpa',
-            'AZ4' => 'November', 'BA4' => 'SPJ', 'BB4' => 'Silpa',
-            'BD4' => 'Desember', 'BE4' => 'SPJ', 'BF4' => 'Silpa'
-        ];
-
-        // Menggabungkan header pertama dan header bulan
-        $headers = array_merge($header1, $months, ['BH4' => 'Total Pagu']);
-
-        // Mengisi nilai sel dengan loop
         foreach ($headers as $cell => $value) {
             $sheet->setCellValue($cell, $value);
         }
 
+        // Style Tabel
+        $styleHeading1 = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FF808080',
+                ],
+            ],
+        ];
+        $styleHeading2 = [
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FFFFA500',
+                ],
+            ],
+        ];
 
-        // sytle    
+        // Rupiah format
+        $rupiahFormat = [
+            'numberFormat' => [
+                'formatCode' => '[$Rp-421] #,##0',
+            ],
+        ];
+        $sheet->getStyle('D17:N17' . ($row - 1))->applyFromArray($rupiahFormat);
+
+        // Looping isi tabel
+        $row = 17;
+        $index = 1;
+
+        // Heading 1
+        foreach ($paguAnggarans as $paguAnggaran) {  
+            $sheet->setCellValue("A$row", $index);
+            $sheet->setCellValue("B$row", $paguAnggaran->kode_rekening);
+            $sheet->getStyle("A$row:N$row")->applyFromArray($styleHeading1);
+            $row++;
+            $index++;
+
+            // Heading 2
+            foreach ($masteAnggarans as $masteAnggaran) {
+                $row++;              
+                $sheet->setCellValue("B$row", $masteAnggaran->kode_rekening);
+                $sheet->getStyle("B$row:N$row")->applyFromArray($styleHeading2);
+                $row++;
+
+                // Heading 3
+                foreach ($groupAnggarans as $groupAnggaran) {  
+                    $sheet->setCellValue("C$row", $groupAnggaran->nama_group);
+                    $sheet->setCellValue("D$row", $groupAnggaran->total_anggaran);
+                    $row++;
+                }
+            }
+        }
+        
+        // Merge Cell Tabel Header
+        $mergeCells = [
+            'A12:A14',
+            'B12:B14',
+            'C12:C14',
+            'D12:E13',
+            'F12:M12',
+            'F13:G13',
+            'H13:K13',
+            'L13:M13',
+            'N12:N14'
+        ];
+        foreach ($mergeCells as $mergeCell) {
+            $sheet->mergeCells($mergeCell);
+        }
+
+        // Autosize
+        foreach (range('A', 'N') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+   
+        // Style Header Tabel
         $headerStyleArray = [
             'font' => [
                 'bold' => true,
-                'color' => ['argb' => 'FFFFFFFF'],
+                'color' => ['argb' => '000000'],
             ],
             'fill' => [
                 'fillType' => Fill::FILL_SOLID,
-                'startColor' => ['argb' => '0F046A'],
+                'startColor' => ['argb' => '4863A0'],
             ],
             'borders' => [
                 'allBorders' => [
-                    'borderStyle' => Border::BORDER_THIN,
+                    'borderStyle' => Border::BORDER_MEDIUM,
                     'color' => ['argb' => 'FF000000'],
                 ],
             ],
             'alignment' => [
                 'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
             ],
         ];
 
-        $sheet->getStyle('A4:BH4')->applyFromArray($headerStyleArray);
-
-        // AutoSize
-        $columns = [
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'X', 'Y', 'Z',
-            'AB', 'AC', 'AD', 'AF', 'AG', 'AH', 'AJ', 'AK', 'AL', 'AN', 'AO', 'AP', 'AR', 'AS', 'AT', 'AV', 'AW', 'AX',
-            'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BH'
-        ];
-
-        foreach ($columns as $columnID) {
-            $sheet->getColumnDimension($columnID)->setAutoSize(true);
-        }
-
-        // data & border
-        $row = 5;
-        $alternate = false;
-        $borderStyleArray = [
-            'borders' => [
-                'allBorders' => [
-                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
-                    'color' => ['argb' => 'FF000000'],
-                ],
+        // Style kode rekening
+        $sheet->getStyle('A17:B' . $sheet->getHighestRow())->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => 12,
             ],
-        ];
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
+            ],
+        ]);
 
-        foreach ($maintenances as $index => $maintenance) {
-            $currentRow = $row + $index;
+        $sheet->getStyle('A12:N14')->applyFromArray($headerStyleArray);
 
-            $sheet->setCellValue('A' . $currentRow, $index + 1)
-                ->setCellValue('B' . $currentRow, $maintenance->kendaraan->nomor_registrasi)
-                ->setCellValue('C' . $currentRow, $maintenance->kendaraan->unitKerja->nama_unit_kerja)
-                ->setCellValue('D' . $currentRow, $maintenance->totalSemuaBelanja())
-                ->setCellValue('E' . $currentRow, \Carbon\Carbon::parse($maintenance->berlaku_sampai)->translatedFormat('d F Y'))
-                ->setCellValue('F' . $currentRow, \Carbon\Carbon::parse($maintenance->tanggal_maintenance)->translatedFormat('F Y'))
-                ->setCellValue('G' . $currentRow, $index)
-                ->setCellValue('H' . $currentRow, $index)
-                ->setCellValue('I' . $currentRow, $index)
-                ->setCellValue('J' . $currentRow, $index)
-                //->setCellValue('K' . $currentRow, $index)
-                ->setCellValue('L' . $currentRow, $index)
-                ->setCellValue('M' . $currentRow, $index)
-                ->setCellValue('N' . $currentRow, $index)
-                //->setCellValue('O' . $currentRow, $index)
-                ->setCellValue('P' . $currentRow, $index)
-                ->setCellValue('Q' . $currentRow, $index)
-                ->setCellValue('R' . $currentRow, $index)
-                //->setCellValue('S' . $currentRow, $index)
-                ->setCellValue('T' . $currentRow, $index)
-                ->setCellValue('U' . $currentRow, $index)
-                ->setCellValue('V' . $currentRow, $index)
-                //->setCellValue('W' . $currentRow, $index)
-                ->setCellValue('X' . $currentRow, $index)
-                ->setCellValue('Y' . $currentRow, $index)
-                ->setCellValue('Z' . $currentRow, $index)
-                //->setCellValue('AA' . $currentRow, $index)
-                ->setCellValue('AB' . $currentRow, $index)
-                ->setCellValue('AC' . $currentRow, $index)
-                ->setCellValue('AD' . $currentRow, $index)
-                //->setCellValue('AE' . $currentRow, $index)
-                ->setCellValue('AF' . $currentRow, $index)
-                ->setCellValue('AG' . $currentRow, $index)
-                ->setCellValue('AH' . $currentRow, $index)
-                //->setCellValue('AI' . $currentRow, $index)
-                ->setCellValue('AJ' . $currentRow, $index)
-                ->setCellValue('AK' . $currentRow, $index)
-                ->setCellValue('AL' . $currentRow, $index)
-                //->setCellValue('AM' . $currentRow, $index)
-                ->setCellValue('AN' . $currentRow, $index)
-                ->setCellValue('AO' . $currentRow, $index)
-                ->setCellValue('AP' . $currentRow, $index)
-                //->setCellValue('AQ' . $currentRow, $index)
-                ->setCellValue('AR' . $currentRow, $index)
-                ->setCellValue('AS' . $currentRow, $index)
-                ->setCellValue('AT' . $currentRow, $index)
-                //->setCellValue('AU' . $currentRow, $index)
-                ->setCellValue('AV' . $currentRow, $index)
-                ->setCellValue('AW' . $currentRow, $index)
-                ->setCellValue('AX' . $currentRow, $index)
-                //->setCellValue('AY' . $currentRow, $index)
-                ->setCellValue('AZ' . $currentRow, $index)
-                ->setCellValue('BA' . $currentRow, $index)
-                ->setCellValue('BB' . $currentRow, $index)
-                //->setCellValue('BC' . $currentRow, $index)
-                ->setCellValue('BD' . $currentRow, $index)
-                ->setCellValue('BE' . $currentRow, $index)
-                ->setCellValue('BF' . $currentRow, $index)
-                //->setCellValue('BG' . $currentRow, $index)
-                ->setCellValue('BH' . $currentRow, $index);
+        // Nama file
+        $writer = new Xlsx($spreadsheet);      
+        $fileName = 'Data Maintenance Kendaraan Dinas Perhubungan Kota Bogor ' . date('d-F-Y') . '.xlsx';
 
-            // koversi ke rupiah
-            $columns = ['H', 'I', 'L', 'M', 'N', 'P', 'Q', 'R'];
-            foreach ($columns as $column) {
-                $sheet->getStyle($column . $currentRow)->getNumberFormat()->setFormatCode('[$Rp-421]#,##0');
-            }
-
-            // warna
-            $fillColor = $alternate ? 'D3D3D3' : 'FFFFFF';
-            $sheet->getStyle("A$currentRow:J$currentRow")->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB($fillColor);
-
-            $sheet->getStyle("A$currentRow:J$currentRow")->applyFromArray($borderStyleArray);
-            $alternate = !$alternate;
-
-
-            // perbulan
-            $months = [
-                'januari'   => ['color' => '98AFC7', 'range' => "L$currentRow:N$currentRow"],
-                'februari'  => ['color' => '728FCE', 'range' => "P$currentRow:R$currentRow"],
-                'maret'     => ['color' => 'B38481', 'range' => "T$currentRow:V$currentRow"],
-                'april'     => ['color' => 'BDEDFF', 'range' => "X$currentRow:Z$currentRow"],
-                'mei'       => ['color' => 'F2D4D7', 'range' => "AB$currentRow:AD$currentRow"],
-                'juni'      => ['color' => '93E9BE', 'range' => "AF$currentRow:AH$currentRow"],
-                'juli'      => ['color' => '00A36C', 'range' => "AJ$currentRow:AL$currentRow"],
-                'agustus'   => ['color' => '64E986', 'range' => "AN$currentRow:AP$currentRow"],
-                'september' => ['color' => 'D462FF', 'range' => "AR$currentRow:AT$currentRow"],
-                'oktober'   => ['color' => 'FFF0DB', 'range' => "AV$currentRow:AX$currentRow"],
-                'november'  => ['color' => 'FFF380', 'range' => "AZ$currentRow:BB$currentRow"],
-                'desember'  => ['color' => 'CA762B', 'range' => "BD$currentRow:BF$currentRow"],
-            ];
-
-            foreach ($months as $month => $data) {
-                $sheet->getStyle($data['range'])->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB($data['color']);
-            }
-
-            // total pagu
-            $fillColor = 'FF8674';
-            $sheet->getStyle("BH$currentRow")->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB($fillColor);
-        }
-
-        $writer = new Xlsx($spreadsheet);
-        $fileName = 'Data Maintenance Kendaraan Dishub Kota Bogor ' . date('d-m-Y') . '.xlsx';
-
-        // headers untuk unduhan file
+        // Headers untuk unduhan file
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="' . $fileName . '"');
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
         exit();
-
-        // composer require phpoffice/phpspreadsheet
     }
 }
