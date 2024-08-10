ff9d355762460601d48a8accb13480f0c5b07ab5
Neddy AP <neddy1298@gmail.com>
2024-08-10 18:53:47 +0700

    chore: Update cron job to use ES modules and TypeScript imports

--- cron.ts
+++ cron.ts
@@ -3,7 +3,7 @@ import shell from 'shelljs';
 
 cron.schedule('* * * * *', () => {
     console.log('running a task every minute');
-    shell.exec('php artisan schedule:run >> /dev/null 2>&1');
+    shell.exec('php artisan schedule:run');
 });
 
 // 0 22 * * 1-5
