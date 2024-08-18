dcd16096d4058f5c4b1ce937d019e1738ad94934
Neddy AP <neddy1298@gmail.com>
2024-08-18 18:05:03 +0700

    Update StokSukuCadangController to remove unused code and improve performance

--- app/Http/Controllers/StokSukuCadangController.php
+++ app/Http/Controllers/StokSukuCadangController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\GroupAnggaran;
 use App\Models\StokSukuCadang;
-use App\Models\SukuCadang;
 use Illuminate\Http\Request;
 
 class StokSukuCadangController extends Controller
@@ -14,37 +13,7 @@ class StokSukuCadangController extends Controller
      */
     public function index()
     {
-        // $sukuCadangs = SukuCadang::all();
         $stokSukuCadangs = StokSukuCadang::orderBy('group_anggaran_id', 'asc')->get();
-        // foreach ($stokSukuCadangs as $stokSukuCadang) {
-        //     $stokSukuCadang->update([
-        //         'stok' => $stokSukuCadang->stok_awal,
-        //     ]);
-        // }
-
-        // foreach ($sukuCadangs as $sukuCadang) {
-        //     $stokSukuCadang = $stokSukuCadangs->where('id', '=', $sukuCadang->stok_suku_cadang_id)->first();
-        //     $stokSukuCadang->update([
-        //         'stok' => $stokSukuCadang->stok - $sukuCadang->jumlah,
-        //     ]);
-        // }
-        
-        // -- Step 1: Add the column as nullable
-        // ALTER TABLE `suku_cadangs` ADD `tanggal_belanja` DATE NULL AFTER `total_harga`;
-
-        // -- Step 2: Update the new column with data from belanjas table
-        // UPDATE suku_cadangs sc
-        // INNER JOIN belanjas b ON sc.belanja_id = b.id
-        // SET sc.tanggal_belanja = b.tanggal_belanja;
-
-        // -- Step 3: Handle any remaining NULL values (if necessary)
-        // UPDATE suku_cadangs
-        // SET tanggal_belanja = '1970-01-01'
-        // WHERE tanggal_belanja IS NULL;
-
-        // -- Step 4: Alter the column to NOT NULL (if required)
-        // ALTER TABLE `suku_cadangs` MODIFY `tanggal_belanja` DATE NOT NULL;
-
         return view('sukuCadang.index', compact('stokSukuCadangs'));
     }
 
