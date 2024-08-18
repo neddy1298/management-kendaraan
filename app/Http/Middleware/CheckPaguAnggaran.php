abc0803b60c0bdddffbb8eb9a42a2ed99cf837d4
Neddy AP <neddy1298@gmail.com>
2024-08-18 18:06:33 +0700

    feat: Update middleware to use cache for counting records
    The middleware CheckMasterAnggaran and CheckPaguAnggaran now use cache to count the records instead of querying the database directly. This improves performance by reducing database queries.

--- app/Http/Middleware/CheckPaguAnggaran.php
+++ app/Http/Middleware/CheckPaguAnggaran.php
@@ -5,20 +5,20 @@ namespace App\Http\Middleware;
 use App\Models\PaguAnggaran;
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Cache;
 
 class CheckPaguAnggaran
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next)
     {
-        if (PaguAnggaran::count() == 0 && !$request->is('paguAnggaran') && !$request->is('paguAnggaran/*')) {
+        $paguAnggaranCount = Cache::remember('pagu_anggaran_count', 60, function () {
+            return PaguAnggaran::count();
+        });
+    
+        if ($paguAnggaranCount == 0 && !$request->is('paguAnggaran') && !$request->is('paguAnggaran/*')) {
             return to_route('paguAnggaran.index')->with('error', 'Silahkan buat Pagu Anggaran terlebih dahulu.');
         }
+    
         return $next($request);
     }
 }
