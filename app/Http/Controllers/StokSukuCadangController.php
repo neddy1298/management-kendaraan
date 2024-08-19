e0ec5f8c78d5230bb405ed2dc1379004adb7f97a
Neddy AP <neddy1298@gmail.com>
2024-08-19 14:56:42 +0700

    chore: Update StokSukuCadangController to synchronize stock with SukuCadang model

--- app/Http/Controllers/StokSukuCadangController.php
+++ app/Http/Controllers/StokSukuCadangController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\GroupAnggaran;
 use App\Models\StokSukuCadang;
+use App\Models\SukuCadang;
 use Illuminate\Http\Request;
 
 class StokSukuCadangController extends Controller
@@ -38,9 +39,24 @@ class StokSukuCadangController extends Controller
     public function update(Request $request, $id)
     {
         $this->validateRequest($request);
-
+        $request['stok_awal'] = $request['stok'];
         $stokSukuCadang = StokSukuCadang::findOrFail($id);
         $stokSukuCadang->update($request->all());
+
+        $sukuCadangs = SukuCadang::all();
+        $stokSukuCadangs = StokSukuCadang::orderBy('group_anggaran_id', 'asc')->get();
+        foreach ($stokSukuCadangs as $stokSukuCadang) {
+            $stokSukuCadang->update([
+                'stok' => $stokSukuCadang->stok_awal,
+            ]);
+        }
+
+        foreach ($sukuCadangs as $sukuCadang) {
+            $stokSukuCadang = $stokSukuCadangs->where('id', '=', $sukuCadang->stok_suku_cadang_id)->first();
+            $stokSukuCadang->update([
+                'stok' => $stokSukuCadang->stok - $sukuCadang->jumlah,
+            ]);
+        }
 
         return redirect()->route('sukuCadang.index')->with('success', 'Data suku cadang berhasil diperbarui');
     }
